bb17b1a3ce89d645607484d1d70881e49e1605ba
Manish Sethi <manish@sethis.in>
2014-09-02 14:13:55 +0000

    Extracting particular node

--- opsview.rb
+++ opsview.rb
@@ -1,11 +1,28 @@
 require 'opsview_rest'
 
 # Connection Establishment
-connection = OpsviewRest.new("http://54.251.90.183/", :username => ENV['OPSVIEW_USERNAME'], :password => ENV['OPSVIEW_PASSWORD'])
+connection = OpsviewRest.new(ENV['SERVER_LINK'], :username => ENV['OPSVIEW_USERNAME'], :password => ENV['OPSVIEW_PASSWORD'])
 
 # Testing Connection
 print "#{connection.inspect}"
 
 # Listing hosts
 
-print "#{connection.list(:type => "host")}"
+def find(options = {})
+    options = {
+     	:type => "host",
+ 	:rows => "all",
+ 	:name => nil
+ 	}.update options
+ 
+    if options[:name].nil?
+      raise ArgumentError, "Need to specify the name of the object."
+    else
+      print"#{get("config/#{options[:type]}?s.name=#{options[:name]}?rows=#{options[:rows]}")}"
+ end
+end 
+
+#print "#{connection.list(:type => "host", :name => "BRMS_Mysql", :ip => "54.85.57.94")}"
+
+#print "#{connection.list(:ip => "54.85.57.94")}"
+
